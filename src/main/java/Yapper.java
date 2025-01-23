0d0cc5acc92b94c83c5311e8d7d1a397a110b8bb
jonzyyyy <jonathan.ngien.zy@gmail.com>
2025-01-23 15:05:58 +0800

    Renamed chatbot to Yapper.java, and added feature to greet user and exits after.

---
+++ src/main/java/Yapper.java
@@ -0,0 +1,13 @@
+public class Yapper {
+    public static void main(String[] args) {
+        String name = "Yapper";
+        int length = 50;
+        String horizontalLine = "-".repeat(length);
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
