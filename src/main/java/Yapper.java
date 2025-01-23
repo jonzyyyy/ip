c6787c5dc3d896f1265253ba63c8398cac2adabb
jonzyyyy <jonathan.ngien.zy@gmail.com>
2025-01-23 15:40:39 +0800

    Added "parrot" response capabilities that reads CLI texts. Also added functionality to end chat. Used OOP concepts.

--- src/main/java/Yapper.java
+++ src/main/java/Yapper.java
@@ -1,13 +1,40 @@
+import java.util.Scanner;
+
 public class Yapper {
+    private static Scanner scanner = new Scanner(System.in);
+    private static String name = "Yapper";
+
     public static void main(String[] args) {
-        String name = "Yapper";
+        printIntroduction();
+        while(true) {
+            String request = scanner.nextLine();
+            if (request.equals("bye")) {
+                break;
+            }
+            printHorizontalLine();
+            System.out.println("\t" + request);
+            printHorizontalLine();
+        }
+        printExit();
+        scanner.close();
+    }
+
+    public static void printIntroduction() {
+        printHorizontalLine();
+        System.out.println("\tHello! I'm " + name);
+        System.out.println("\tWhat can I do for you?");
+        printHorizontalLine();
+    }
+
+    public static void printHorizontalLine() {
         int length = 50;
         String horizontalLine = "-".repeat(length);
-        System.out.println(horizontalLine);
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
-        System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+        System.out.println("\t" + horizontalLine);
+    }
+
+    public static void printExit() {
+        printHorizontalLine();
+        System.out.println("\tBye. Hope to see you again soon!");
+        printHorizontalLine();
     }
 }
